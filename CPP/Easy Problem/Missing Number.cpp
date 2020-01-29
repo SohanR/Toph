f171143a9a167cb3f068e84b9ffa0ba4666014fa
Mizanur Rahman <mizan.rahman66d@gmail.com>
2020-01-29 21:06:12 +0600

    mssing number

---
+++ CPP/Easy Problem/Missing Number.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+
+using namespace std;
+int main()
+{
+    int sum,a1,a2,a3,n,m;
+    n=m=0;
+    cin>>sum;
+    cin>>a1>>a2>>a3;
+
+    n=a1+a2+a3;
+    m=sum-n;
+
+    cout<<m;
+    return 0;
+}
