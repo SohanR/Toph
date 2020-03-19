f23707328c773ecd60aa6bb6b9bbdabdaa59210b
SohanR <mizan.rahman66d@gmail.com>
2020-03-19 15:25:57 +0600

    Mixed fractions

---
+++ CPP/Mathematics/Mixed Fractions.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,div,mod;
+
+    cin>>a;
+    cin>>b;
+
+    div=a/b;
+    mod=a%b;
+    cout<<div<<" "<<mod<<" "<<b<<endl;
+return 0;
+}
