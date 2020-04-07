7e07ae67a19285522c63bd2eb6605b1fc3ea9895
SohanR <mizan.rahman66d@gmail.com>
2020-04-07 22:15:35 +0600

    average again

---
+++ CPP/Easy Problem/Running Average Again.cpp
@@ -0,0 +1,36 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+
+int main()
+{
+    int i,j=1,N;
+    double evrg=0,x=0;
+
+
+    cin>>N;
+    
+    if(N<=100000)
+    {
+        float a[N];
+
+        for(i=0;i<N;i++)
+        {
+            cin>>a[i];
+        }
+        
+    
+        for(i=0;i<N;i++)
+        {
+            x+=a[i];
+
+            evrg=x/j;
+            j++;
+            
+            cout<<evrg<<fixed<<setprecision(9)<<endl;
+        }
+        
+        
+    }
+return 0;
+}
