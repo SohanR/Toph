d66b6e4e2e9eb5cac0fc0f0dc3f6ea2bea46aa13
SohanR <mizan.rahman66d@gmail.com>
2020-04-25 13:35:17 +0600

    reverse

---
+++ String/CplusPlus/Is Palindrome.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    string s, r;
+
+    cin >> s;
+
+    r = s;
+    // int start = 0;
+    // int end = s.size() - 1;
+
+    // while (start < end)
+    // {
+    //     if (s[start++] != s[end--])
+    //     {
+    //         cout << "No" << endl;
+    //         break;
+    //     }
+    // }
+    // cout << "YES" << endl;
+
+    reverse(r.begin(), r.end());
+    if (r == s)
+    {
+        cout << "YES" << endl;
+    }
+    else
+    {
+        cout << "NO" << endl;
+    }
+
+    return 0;
+}
